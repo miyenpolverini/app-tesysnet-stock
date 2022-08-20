13cb0d4de8bc58936f98af348a106e156534cf41
Miyen Polverini <miyenpolverini@gmail.com>
2022-08-20 20:58:39 -0300

    version final

--- src/Components/InfoEvento/InfoEvento.js
+++ src/Components/InfoEvento/InfoEvento.js
@@ -10,7 +10,7 @@ const InfoEvento = () => {
                 <div>
                     <h4 className='detail-tit mt-3'>CEREMONIA</h4>
                     <h5 className='detail-info-tit mt-5'>Dirección</h5>
-                    <h6 className='detail-info'>Iglesia Nuestra Señora de Fátima, colectora Ruta 28 y acceso oeste</h6>
+                    <h6 className='detail-info'>Iglesia Nuestra Señora de Fátima - Colectora Ruta 28 y acceso oeste</h6>
                     <h5 className='detail-info-tit mt-3'>Localidad</h5>
                     <h6 className='detail-info'>General Rodriguez</h6>
                     <h5 className='detail-info-tit mt-3'>Provincia</h5>
@@ -19,14 +19,14 @@ const InfoEvento = () => {
                     <h6 className='detail-info mb-5'>21 de Octubre 2022 - 18:00hs</h6>
                 </div>
                 <div>
-                    <iframe className='contMaps' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13138.04521917269!2d-58.9521968!3d-34.5912301!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcc11a1a80c9326f4!2sIglesia%20Nuestra%20Se%C3%B1ora%20de%20F%C3%A1tima!5e0!3m2!1ses-419!2sar!4v1660949760812!5m2!1ses-419!2sar" width="600" height="450" title='mapa2' allowfullscreen="" loading="lazy"></iframe>
+                    <iframe className='contMaps' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13138.04521917269!2d-58.9521968!3d-34.5912301!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcc11a1a80c9326f4!2sIglesia%20Nuestra%20Se%C3%B1ora%20de%20F%C3%A1tima!5e0!3m2!1ses-419!2sar!4v1660949760812!5m2!1ses-419!2sar" title='mapa2' allowfullscreen="" loading="lazy"></iframe>
                 </div>
             </div>
             <div id='evento-fiesta' className='box-evento'>
                 <div>
                     <h4 className='detail-tit '>FIESTA</h4>
                     <h5 className='detail-info-tit mt-5'>Dirección</h5>
-                    <h6 className='detail-info'>Janos Hurlingham, gral Pedro Díaz 1800</h6>
+                    <h6 className='detail-info'>Janos Hurlingham - Gral Pedro Díaz 1800</h6>
                     <h5 className='detail-info-tit mt-3'>Localidad</h5>
                     <h6 className='detail-info'>Hurlingham</h6>
                     <h5 className='detail-info-tit mt-3'>Provincia</h5>

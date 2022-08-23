a4f5dc24b7682f699051e22d3de5e458aa5874d6
Miyen Polverini <miyenpolverini@gmail.com>
2022-08-22 21:47:54 -0300

    version final

--- src/Components/ItemIndex/ItemIndex.js
+++ src/Components/ItemIndex/ItemIndex.js
@@ -8,9 +8,9 @@ import videoInvitacion from '../video/videoInvitacion.mp4';
 const ItemIndex = () => {
     return (
         <div>
-             <video className='img-inicio'  preload='auto' muted autoPlay src={videoInvitacion}></video>
-            
-         {/*    <img className='img-inicio' alt='foto portada' src='https://res.cloudinary.com/dw94zgfgu/image/upload/v1648512794/WhatsApp_Image_2022-03-28_at_9.12.50_PM_kylitz.jpg'></img> */}
+            <video className='img-inicio' preload='auto' muted autoPlay src={videoInvitacion}></video>
+
+            {/*    <img className='img-inicio' alt='foto portada' src='https://res.cloudinary.com/dw94zgfgu/image/upload/v1648512794/WhatsApp_Image_2022-03-28_at_9.12.50_PM_kylitz.jpg'></img> */}
             <div className='img-fondo'></div>
 
             <div id='formulario-asistencia' className='countdown-portada' uk-countdown="date: 2022-10-21T03:10:16+00:00">
@@ -30,10 +30,18 @@ const ItemIndex = () => {
                     <div className="uk-countdown-number uk-countdown-seconds count-det "></div>
                 </div>
             </div>
- 
-            <iframe className='formulario-asistencia' src="https://docs.google.com/forms/d/e/1FAIpQLSduObXdLOEsNOO4aIB_j8a7NDvGRayxiRDYGDh-LtT6fED44A/viewform?embedded=true" title='formu-asistencia' frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
 
-            <h4 className='parrafo2-portada'>Te esperamos en la...</h4>
+            {/*     <iframe className='formulario-asistencia' src="https://docs.google.com/forms/d/e/1FAIpQLSduObXdLOEsNOO4aIB_j8a7NDvGRayxiRDYGDh-LtT6fED44A/viewform?embedded=true" title='formu-asistencia' frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe> */}
+
+            <h4 className='parrafo2-portada'>CONFIRMAR ASISTENCIA</h4>
+            <h6 className='parrafo2-subtit'>Contamos con tu presencia en este día tan especial</h6>
+            <a className="social-icons__link" href="https://forms.gle/F17onPWsiJDhppCcA" target="_blank" rel='noreferrer'>
+                <button className='botonLlegar'>
+                    RSVP<span className='btn-arrow'><IoIosArrowForward /></span>
+                </button>
+            </a>
+
+            <h4 className='parrafo3-portada'>TE ESPERAMOS EN LA...</h4>
             <div className='parrafo2-div'>
                 <div>
                     <h5 className='parrafo2-contenido'>CEREMONIA</h5>
@@ -64,7 +72,9 @@ const ItemIndex = () => {
             </div>
             <video className='video-portada' preload='auto' muted loop autoPlay src='https://video.wixstatic.com/video/11062b_7488edba38234bd69b0603ad498efdf5/720p/mp4/file.mp4'></video>
             <h5 className='tit-final'>¡Estamos muy contentos de poder compartir este día tan importante con ustedes!</h5>
-            <img className='img-final' alt='foto nosotros' src='https://res.cloudinary.com/dw94zgfgu/image/upload/v1661126675/WhatsApp_Image_2022-08-21_at_8.58.28_PM_dzltro.jpg'></img>
+            <div className='img-final-contenido'>
+                <img className='img-final' alt='foto nosotros' src='https://res.cloudinary.com/dw94zgfgu/image/upload/v1661126675/WhatsApp_Image_2022-08-21_at_8.58.28_PM_dzltro.jpg'></img>
+            </div>
             {/* <img className='img-final' alt='foto final' src='https://res.cloudinary.com/dw94zgfgu/image/upload/v1648488802/Flu_miyo_na7vzr.png'></img> */}
         </div>
     )

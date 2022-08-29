39775d5bc15989907c827466b2163957dc5e9231
Miyen Polverini <miyenpolverini@gmail.com>
2022-08-28 22:42:43 -0300

    version final

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -20,8 +20,8 @@ const NavBar = () => {
                         <h3 className='logoWedding'>#FLU & MIYO</h3>
                     </HashLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        {/* <span className="navbar-toggler-icon"></span> */}
-                        <Hamburger toggled={isOpen} toggle={setOpen} />
+                       {/*  <span className="navbar-toggler-icon"></span> */}
+                  <Hamburger size={24} toggled={isOpen} toggle={setOpen} /> 
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul className="navbar-nav">
